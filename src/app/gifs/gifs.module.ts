00dc782ea6293d8651de6aeddd5813eac5e38ecb
Sergio Ortiz <sergio.ortiz@visma.com>
2022-09-23 18:46:09 +0200

    Servicio búsqueda gifs

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPageComponent,
+    BusquedaComponent,
+    ResultadosComponent
+  ],
+  exports: [
+    GifsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
