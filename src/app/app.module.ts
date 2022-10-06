188d3e46cbdfd8c87adecca9b728d779dca6902a
Sergio Ortiz <sergio.ortiz@visma.com>
2022-10-06 20:50:58 +0200

    Conexion API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,8 +12,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
-    GifsModule
+    GifsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  private apiKey: string = 'VYLAbGgRGbxyI9knVRyokfE2fRvSP7NL';
   private _historial: string[] = [];
+
+  public resultados: Gif[] = []
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string = '') {
 
@@ -18,6 +26,25 @@ export class GifsService {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
-    console.log(this._historial);
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=VYLAbGgRGbxyI9knVRyokfE2fRvSP7NL&q=${query}&limit=10`)
+      .subscribe((resp) => {
+        this.resultados = resp.data;
+      })
+
+
+
+    // Opción Vanilla JS de conexión a la API
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=VYLAbGgRGbxyI9knVRyokfE2fRvSP7NL&q=patata&limit=10')
+    //   .then(resp => {
+    //     resp.json().then(data => console.log(data))
+    //   })
+
+    // Opción mejorada Vanilla JS de conexión a la API - poner async en el método
+    // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=VYLAbGgRGbxyI9knVRyokfE2fRvSP7NL&q=patata&limit=10');
+    // const data = await resp.json();
+    // console.log(data)
+
+
   }
 }
